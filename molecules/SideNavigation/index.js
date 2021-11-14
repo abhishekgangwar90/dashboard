bca03f9eabb1cfaab5e4e6925e127d107aa78a0a
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-11-14 23:06:30 +0530

    feat: added navigation link changes

--- molecules/SideNavigation/index.js
+++ molecules/SideNavigation/index.js
@@ -2,17 +2,18 @@ import Stack from 'react-bootstrap/Stack';
 import { Row , Col} from 'react-bootstrap';
 import { appConfig } from '../../config/app.config';
 
+import styles from './sideNavigation.module.scss';
 
 
 function NavLink({ name, icon}){
-  return <Row>
-      <Col md="1" className="u-margin-right-small">{icon}</Col>
-      <Col>{name}</Col>
-    </Row>
+  return <div className={styles.navLink}>
+      <span className="u-margin-right-small">{icon}</span>
+      <span>{name}</span>
+    </div>
 }
 
 function SideNavigation(){
-  return <Stack gap={4} className="u-padding-small">
+  return <Stack gap={4} className={`u-padding-small ${styles.sideNav}`}>
     {appConfig.sideNavigation
       .filter(res => res.accessType === appConfig.accessType)
       .map((res) => {
